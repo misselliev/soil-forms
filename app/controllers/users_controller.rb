e24d7728f5665b08d80e0afac9334fa37b3ac03e
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-05-15 18:46:36 -0700

    Editing users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,19 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update user_params
+      redirect_to new_users_path
+    else
+      render :new    
+      
+  end
+
   private
 
   def user_params
